fb3317452f70998ea14d8c26eaa81d0e04d4f584
NNoorFatima <50588372+NNoorFatima@users.noreply.github.com>
2023-11-04 18:46:45 +0500

    bst.cpp

---
+++ bst.cpp
@@ -0,0 +1,29 @@
+
+// BST.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include "BST.h"
+using namespace std;
+int main()
+{
+    BST a;
+    a.insert(50);
+    a.insert(30);
+    a.insert(10);
+    a.insert(80);
+    a.insert(60);
+    a.insert(90);
+    a.insert(110);
+    a.insert(85);
+    a.insert(83);
+    a.insert(95);
+    a.insert(92);
+    a.insert(40);
+    a.insert(45);
+    a.insert(39);
+    a.deleteData(90);
+    a.deleteData(30);
+    a.insert(55);
+    cout<<a.search(5);
+
+}
